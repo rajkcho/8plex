c199995b6b1549cfe700ecc7be1c08cd9c792bc2
Rajan Chopra <rajanchopra@rajans-air.lan>
2025-11-14 18:07:18 -0500

    chore: scaffold Vite React app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>8-Plex Dashboard</h1>
+      <p>The interactive underwriting experience is coming soon.</p>
+    </div>
+  );
+}
+
+export default App;
